0dd05555e113434196ea12405145cd2414624be0
Mateus <trombetta.mateus@gmail.com>
2023-06-17 16:58:48 -0300

    Created Bullet prefab and behaviour

---
+++ Assets/Code/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private const float BULLET_SPEED = 10f;
+
+    private void Update()
+    {
+        transform.position += transform.up * Time.deltaTime * BULLET_SPEED;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
