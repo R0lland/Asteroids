566689d95e540898b4886254f7b31b7368f96c93
Mateus <trombetta.mateus@gmail.com>
2023-06-18 16:45:15 -0300

    Added teleportes boundaries to teleport player and enemies to the other side

---
+++ Assets/Code/Scripts/Game/Teleporter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] private float _offset;
+    [SerializeField] private Transform _teleportTo;
+    [SerializeField] private bool _teleportingOnX;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.LogError("Trigger");
+        if (_teleportingOnX)
+        {
+            collision.transform.position = new Vector3(_teleportTo.position.x + _offset, collision.transform.position.y, collision.transform.position.z);
+        }
+        else
+        {
+            collision.transform.position = new Vector3(collision.transform.position.x, _teleportTo.position.y + _offset, collision.transform.position.z);
+        }
+       
+    }
+}
